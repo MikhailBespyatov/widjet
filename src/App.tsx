57267be6b17a225f5a2b3b9d34a92154f2197609
mikhail <besp9tov.mikhail94@gmail.com>
2022-08-03 17:01:41 +0600

    feat(core): add preapp id fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-export const App = ({message}: {message: string}) => {
-  return <div style={{fontSize: '48px'}}>{message}</div>
+import { useEffect, useState } from "react"
+
+export const App = ({preappValues}: {preappValues: any}) => {
+  const [data, setData] = useState()
+  console.log(preappValues)
+  useEffect((
+  ) => {
+    fetch("preapp", {method: "POST", body: JSON.stringify(preappValues) }).then((res) => console.log(res))
+  }, [])
+  return <div style={{fontSize: '48px'}}>Hello bnpl</div>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import { createRoot, Root } from 'react-dom/client';
 
 let root = createRoot(document.getElementById('bnpl-root')!);;
 
-export const render = (message: string) => {
+export const render = (preappValues: any) => {
   if (root) {
     root = createRoot(document.getElementById('bnpl-root')!);
   }
   root.render(
-    <App message={message}/>
+    <App preappValues={preappValues}/>
   )
 };
 
