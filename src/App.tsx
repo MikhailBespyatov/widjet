f3fea9ddf0f76699e4f879223d6c8216d910e0d2
mikhail <besp9tov.mikhail94@gmail.com>
2022-08-03 17:10:20 +0600

    fix(core): fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react"
 export const App = ({preappValues}: {preappValues: any}) => {
   const [data, setData] = useState()
   console.log(preappValues)
+  const baseUrl = process.env.NODE_ENV === 'development' ? '/proxy/' : '/bnpl/';
   useEffect((
   ) => {
-    fetch("preapp", {method: "POST", body: JSON.stringify(preappValues) }).then((res) => console.log(res))
+    fetch("/bnpl/preapp", {method: "POST", body: JSON.stringify(preappValues) }).then((res) => console.log(res))
   }, [])
   return <div style={{fontSize: '48px'}}>Hello bnpl</div>
 }
